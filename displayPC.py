3fca8d74679533074ddf581cdc2d0d8eecaf6d23
Yuto Nakano <yuto.44.bb@gmail.com>
2019-03-09 20:17:31 +0900

    add UIVar Button

--- displayPC.py
+++ displayPC.py
@@ -1,16 +1,27 @@
 import tkinter
+from tkinter import *
+from tkinter import ttk
 class Base(tkinter.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        # ウィンドウサイズ
+        master.geometry("900x720")
+        master.title("伝説のあの人とついに会話できるようになりました。")
+
+        
+
         # 入力用テキストボックス
         self.entryBox = tkinter.Entry(master=self)
         self.entryBox.pack()
         # テキストボックスの内容を表示するボタン
-        self.button1 = tkinter.Button(master=self, text='show', width=20)
-        self.button1.bind("<ButtonRelease-1>", self.show_word)
+        self.button1 = tkinter.Button(master=self, text='テキストを送信する', width=30, bg='#5DB529')
+        self.button1.bind("<ButtonRelease-1>", self.send_word)
         self.button1.pack()
-    def show_word(self, event):
-        print(self.entryBox.get())
+    # def value_changed(*args):
+    #     print('value = %d' % myval.get())
+
+
+
 if __name__ == '__main__':
     root = tkinter.Tk()
     f = Base(master=root)
