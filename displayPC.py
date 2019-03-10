d242d1aeebc4e07af2976ace3a7da66a00abb698
Yuto Nakano <yuto.44.bb@gmail.com>
2019-03-10 12:04:20 +0900

    get scroll value

--- displayPC.py
+++ displayPC.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 
+
+
 class Base(tk.Frame):
+    pitch_value = 0
+    speed_value = 0
+    volume_value = 0
     def __init__(self, master=None):
         super().__init__(master)
         master.geometry("1200x1000")
@@ -66,30 +71,27 @@ class Base(tk.Frame):
 
         # pitch
         c = tk.Label(text="テンポ")
-        c.place(x=500, y=400)
+        c.place(x=500, y=390)
         self.pitch = tk.DoubleVar(master=self)
-        self.scale = tk.Scale(master=self, variable=self)
-        self.scale.pack(anchor=tk.CENTER)
-        self.button = tk.Button(master=self, text="Get Scale Value")
-        self.button.pack(anchor=tk.CENTER)
+        # self.scale1 = tk.Scale(master=self, variable=self)
+        self.scale1 = tk.Scale(master=self,command=self.pitch_value)
+        self.scale1.pack(anchor=tk.CENTER)
+
 
         # speed
         a = tk.Label(text='速さ')
-        a.place(x=500, y=530)
+        a.place(x=500, y=480)
         self.speed = tk.DoubleVar(master=self)
-        self.scale = tk.Scale(master=self, variable=self)
-        self.scale.pack(anchor=tk.CENTER)
-        self.button = tk.Button(master=self, text="Get Scale Value")
-        self.button.pack(anchor=tk.CENTER)
+        self.scale2 = tk.Scale(master=self,command=self.speed_value)
+        self.scale2.pack(anchor=tk.CENTER)
 
         # volume
         b = tk.Label(text='ボリューム')
-        b.place(x=500, y=660)
+        b.place(x=500, y=580)
         self.volume = tk.DoubleVar(master=self)        
-        self.scale = tk.Scale(master=self, variable=self)
-        self.scale.pack(anchor=tk.CENTER)
-        self.button = tk.Button(master=self, text="Get Scale Value")
-        self.button.pack(anchor=tk.CENTER)
+        self.scale3 = tk.Scale(master=self,command=self.volume_value)
+        self.scale3.pack(anchor=tk.CENTER)
+
 
         # 音声再生ボタン
         self.hi_there = tk.Button(self)
@@ -97,6 +99,16 @@ class Base(tk.Frame):
         self.hi_there["command"] = self.say
         self.hi_there.pack(side="top")
 
+
+    def speed_value(self, val):
+        print(val)
+        speed_val = val
+    def pitch_value(self, val):
+        print(val)
+        pitch_val = val
+    def volume_value(self, val):
+        print(val)
+        volume_val = val
 
     def say(self):
         print("音声を流す")
@@ -107,14 +119,15 @@ class Base(tk.Frame):
     def send_word(self, event):
         print(self.entryBox.get())
         print(self.volume.get())
+
         params = {
             "text": self.entryBox.get(),     # 200文字以内
             "speaker": "santa",                                         # 話者名
             "emotion": "happiness",                                     # 感情
             "emotion_level": 4,                                         # 感情レベル
-            "pitch": self.pitch.get(),                                               # 音の高さ
-            "speed": self.speed.get(),                                                # 音声の速度
-            "volume": self.volume.get()                                               # 音声の大きさ
+            "pitch": self.pitch_value,                                               # 音の高さ
+            "speed": self.speed_value,                                                # 音声の速度
+            "volume": self.volume_value                                               # 音声の大きさ
         }
         print(params)
 
